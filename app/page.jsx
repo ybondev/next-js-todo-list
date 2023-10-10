0807b5c43d872815d9658f23062fd74cd12c375c
ybondev <ybon.dev@gmail.com>
2023-10-10 14:16:12 +0800

    changes

--- app/page.jsx
+++ app/page.jsx
@@ -85,71 +85,75 @@ const page = () => {
   };
 
   return (
-    <section className="container todo_container">
-      <div className="row gy-3">
-        <div className="col-md-6">
-          <Image
-            src="./assets/undraw_online_calendar_re_wk3t.svg"
-            alt="undraw_online_calendar_re_wk3t.svg"
-            width={500}
-            height={0}
-            className="img-fluid"
-          />
-        </div>
-        <div className="col-md-6">
-          <form className="form_container" onSubmit={handleSubmit}>
-            <h1>
-              Todo <span>list</span>
-            </h1>
-            <div className="input_container mt-3 mb-3">
-              <input
-                type="text"
-                id="input"
-                className="form-control"
-                placeholder="Enter here..."
-                value={value}
-                onChange={(e) => setValue(e.target.value)}
-              />
-              <AiOutlinePlus className="fa_icon" onClick={addItem} />
-            </div>
-            <div className="overflow">
-              {items?.map((list, index) => {
-                return (
-                  <div className="data_container mt-2" key={index}>
-                    <div
-                      className={`display ${list.completed ? "complete" : ""}`}
-                    >
-                      <p className={`${list.completed ? "complete" : ""}`}>
-                        {list.name}
-                      </p>
-                      <div className="icon">
-                        <AiOutlineCheckCircle
-                          className="fa_icon"
-                          onClick={() => handleCheck(list)}
-                        />
-                        <AiOutlineDelete
-                          className="fa_icon"
-                          onClick={() => deleteItem(index)}
-                        />
+    <>
+      <section className="container todo_container">
+        <div className="row gy-3">
+          <div className="col-md-6">
+            <Image
+              src="./assets/undraw_online_calendar_re_wk3t.svg"
+              alt="undraw_online_calendar_re_wk3t.svg"
+              width={500}
+              height={0}
+              className="img-fluid"
+            />
+          </div>
+          <div className="col-md-6">
+            <form className="form_container" onSubmit={handleSubmit}>
+              <h1>
+                Tracker <span>list</span>
+              </h1>
+              <div className="input_container mt-3 mb-3">
+                <input
+                  type="text"
+                  id="input"
+                  className="form-control"
+                  placeholder="Enter here..."
+                  value={value}
+                  onChange={(e) => setValue(e.target.value)}
+                />
+                <AiOutlinePlus className="fa_icon" onClick={addItem} />
+              </div>
+              <div className="overflow">
+                {items?.map((list, index) => {
+                  return (
+                    <div className="data_container mt-2" key={index}>
+                      <div
+                        className={`display ${
+                          list.completed ? "complete" : ""
+                        }`}
+                      >
+                        <p className={`${list.completed ? "complete" : ""}`}>
+                          {list.name}
+                        </p>
+                        <div className="icon">
+                          <AiOutlineCheckCircle
+                            className="fa_icon"
+                            onClick={() => handleCheck(list)}
+                          />
+                          <AiOutlineDelete
+                            className="fa_icon"
+                            onClick={() => deleteItem(index)}
+                          />
+                        </div>
                       </div>
                     </div>
-                  </div>
-                );
-              })}
+                  );
+                })}
+              </div>
+            </form>
+            <div className="btn_delete">
+              <div></div>
+              <button onClick={handleDeleteAll}>delete all</button>
             </div>
-          </form>
-          <div className="btn_delete">
-            <div></div>
-            <button onClick={handleDeleteAll}>delete all</button>
-          </div>
-          <div className="dev">
-            <Link href="" className="link">
-              ybon.dev
-            </Link>
+            <div className="dev">
+              <Link href="" className="link">
+                ybon.dev
+              </Link>
+            </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 };
 
